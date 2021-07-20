986e2f57b7d2e8a3bd389d3a69f1210b5d8d3be1
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-07-20 16:49:15 +0200

    WIP: Added a full build script with order according to dependencies.

--- build.sh
+++ build.sh
@@ -1,1 +1,19 @@
+target=$1
+debug=${2:-"release"}
+fast=${3:-"fast"}
 
+cd components/OASIS3-MCT
+#./build.sh "$target" "$debug" "$fast"
+cd -
+
+cd components/flux_calculator
+./build.sh "$target" "$debug" "$fast"
+cd -
+
+cd components/CCLM
+./build.sh "$target" "$debug" "$fast"
+cd -
+
+cd components/MOM5
+./build.sh "$target" "$debug" "$fast"
+cd -
