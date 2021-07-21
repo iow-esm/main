87d9a1b0ef377fe8010065a1a6915e3be0b2ff90
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-07-21 15:52:14 +0200

    Enabled running on HLRN in Göttingen

---
+++ local_scripts/run_hlrng.sh
@@ -0,0 +1,10 @@
+user_at_dest=$1
+dest_folder=$2
+
+python_module="anaconda3/2019.03"
+
+echo ssh -t $user_at_dest "module load ${python_module}; cd $dest_folder/scripts/prepare; python3 ./create_jobscript.py"
+ssh -t $user_at_dest "module load ${python_module}; cd $dest_folder/scripts/prepare; python3 ./create_jobscript.py"
+
+echo ssh -t $user_at_dest "cd $dest_folder/scripts/run; source ~/.bash_profile; sbatch jobscript"
+ssh -t $user_at_dest "cd $dest_folder/scripts/run; source ~/.bash_profile; sbatch jobscript"
