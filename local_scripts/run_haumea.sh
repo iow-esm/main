f01bf6a7ec4acb67d07b87a6c5283cbef8728fb7
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-07-21 15:45:20 +0200

    Enabled automatic creation of jobscript file.
    When the model run is started the jobscript file is automatically updated.

--- local_scripts/run_haumea.sh
+++ local_scripts/run_haumea.sh
@@ -1,5 +1,10 @@
 user_at_dest=$1
 dest_folder=$2
 
+python_module="miniconda3/4.8.2"
+
+echo ssh -t $user_at_dest "source ~/.bash_profile; module load ${python_module}; cd $dest_folder/scripts/prepare; python3 ./create_jobscript.py"
+ssh -t $user_at_dest "source ~/.bash_profile; module load ${python_module}; cd $dest_folder/scripts/prepare; python3 ./create_jobscript.py"
+
 echo ssh -t $user_at_dest "cd $dest_folder/scripts/run; source ~/.bash_profile; sbatch jobscript"
 ssh -t $user_at_dest "cd $dest_folder/scripts/run; source ~/.bash_profile; sbatch jobscript"
