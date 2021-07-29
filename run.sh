5203a71d117995d8041c7816c164d87db1ab66a2
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-07-29 12:58:30 +0200

    Made run.sh a bit more verbose.

--- run.sh
+++ run.sh
@@ -10,10 +10,25 @@ else
 	target=$1
 fi	
 
+
+echo "###############################################"
+echo "##                                           ##"
+echo "##          IOW earth-system model           ##"
+echo "##                                           ##"
+echo "###############################################"
+echo ""
+echo "###############################################"
+echo ""
+
+
 #available_args=("target" "update_from_local_setup")
 #source ./local_scripts/parse_args.sh "${available_args[@]}" "," "$@"
 
+echo "##       Identify the target of the run      ##"
+echo "###############################################"
 source ./local_scripts/identify_target.sh $target
+echo ""
+echo ""
 
 # check if there are more optional arguments
 if [ $# -gt 1 ]; then
@@ -23,25 +38,51 @@ if [ $# -gt 1 ]; then
 	# first optional argument: should we run prepare scripts before we run?
 	prepare_arg=1
 	
+	echo "##     Check if model sould be prepared      ##"
+	echo "###############################################"
 	if [ "${args[${prepare_arg}]}" == 'prepare-before-run' ]; then
 		setups_arg=2				#if yes, possible setup-update arguments will follow at index 2
 		prepare_before_run=true
+		echo "The model will be prepared before the run."
 	else
 		setups_arg=1				#if no, possible setup-update arguments will follow at index 1
 		prepare_before_run=false
+		echo "The model will not be prepared before the run."
 	fi
+	echo ""
+	echo ""
 	
+	echo "##   Check if the setup will be updated      ##"
+	echo "###############################################"
 	for ((i = ${setups_arg}; i < $#; i++)); do 
+		echo "The setup will be updated."
 		./deploy_setups.sh "$target" "${args[i]}"
 	done
+	echo ""
+	echo ""
 fi
 
+echo "##        Sync scripts to the target         ##"
+echo "###############################################"
 ./local_scripts/sync_scripts.sh ${user_at_dest} ${dest_folder}
+echo ""
+echo ""
 
+echo "##        Gather info on last build          ##"
+echo "###############################################"
 ./local_scripts/tag_build.sh ${target} "" ""
+cat "LAST_BUILD_$target"
+echo ""
+echo "Transfer it to the target:"
 echo scp "LAST_BUILD_$target" ${user_at_dest}:${dest_folder}/
 scp "LAST_BUILD_$target" ${user_at_dest}:${dest_folder}/
+echo ""
+echo ""
 
+echo "##         Start the run on target           ##"
+echo "###############################################"
 ./local_scripts/run_${target}.sh ${user_at_dest} ${dest_folder} ${prepare_before_run}
+echo ""
+echo ""
 
 
