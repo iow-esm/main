084c9cc31e6b7b7ea3732d45115523f0e8b79d59
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-08-02 16:05:02 +0200

    Improved build.sh

--- build.sh
+++ build.sh
@@ -2,18 +2,15 @@ target=$1
 debug=${2:-"release"}
 fast=${3:-"fast"}
 
-cd components/OASIS3-MCT
-./build.sh "$target" "$debug" "$fast"
-cd -
-
-cd components/flux_calculator
-./build.sh "$target" "$debug" "$fast"
-cd -
-
-cd components/CCLM
-./build.sh "$target" "$debug" "$fast"
-cd -
-
-cd components/MOM5
-./build.sh "$target" "$debug" "$fast"
-cd -
+components=(
+	"OASIS3-MCT"
+	"flux_calculator"
+	"CCLM"
+	"MOM5"
+)
+	
+for c in "${components[@]}"; do
+	cd components/"$c"
+	./build.sh "$target" "$debug" "$fast"
+	cd -
+done
