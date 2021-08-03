24e42adbb97a7f6542f3d6e06d4d7e42fbc8322f
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-08-03 13:32:43 +0200

    Enabled advanced destination keywords

--- local_scripts/identify_target.sh
+++ local_scripts/identify_target.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+available_targets=(
+	"hlrnb"
+	"hlrng"
+	"haumea"
+	"phy-2"
+)
+
 if [ $# -lt 1 ]; then
 	echo "Put a target from ../DESTINATIONS as first argument"
 	exit
 fi
 
-target=$1
+target_keyword=$1
 debug=${2:-"release"} # debug/release
 fast=${3:-"fast"}	# fast/rebuild
 
+target=`printf '%s\n' "${available_targets[@]}" | grep -w "${target_keyword%%_*}"`
+if [ -z ${target} ]; then
+	echo "The target ${target_keyword%_*} is not supported"
+	exit
+fi
+
 local="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-dest="`awk -v dest="$target" '{if($1==dest){print $2}}' ${local}/../DESTINATIONS`"
+dest="`awk -v dest="$target_keyword" '{if($1==dest){print $2}}' ${local}/../DESTINATIONS`"
 
 if [ -z $dest ]; then
 	echo "Unknown destination"
