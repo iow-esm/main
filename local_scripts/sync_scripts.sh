45cf6fd67469b0fe72015aa9067151e556ce474a
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-09-07 08:51:36 +0200

    Changed verbosity of rsync

--- local_scripts/sync_scripts.sh
+++ local_scripts/sync_scripts.sh
@@ -7,7 +7,7 @@ echo ssh -t $user_at_dest \"mkdir -p $dest_folder/scripts/run\"
 ssh -t $user_at_dest "mkdir -p $dest_folder/scripts/prepare"
 ssh -t $user_at_dest "mkdir -p $dest_folder/scripts/run"
 
-echo rsync -avz -u ./scripts/prepare/ $dest/scripts/prepare
-echo rsync -avz -u ./scripts/run/ $dest/scripts/run
-rsync -avz -u ./scripts/prepare/ $dest/scripts/prepare
-rsync -avz -u ./scripts/run/ $dest/scripts/run
+echo rsync -r -i -u ./scripts/prepare/ $dest/scripts/prepare
+echo rsync -r -i -u ./scripts/run/ $dest/scripts/run
+rsync -r -i -u ./scripts/prepare/ $dest/scripts/prepare
+rsync -r -i -u ./scripts/run/ $dest/scripts/run
