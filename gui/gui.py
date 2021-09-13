d00bbdfece07cadcecd7ec082f5018c58dda0554
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-09-13 16:22:52 +0200

    Introduced IOW colors

--- gui/gui.py
+++ gui/gui.py
@@ -12,6 +12,11 @@ from functools import partial
 
 root_dir = "."
 
+class IowColors:
+    blue1 = "#0a2b5a"
+    green1 = "#74e40d"
+    grey1 = "#4e584e"
+
 class IowEsmFunctions:
     def __init__(self, gui):
         self.gui = gui
@@ -107,8 +112,8 @@ class FunctionButton(tk.Button):
         tk.Button.__init__(self,
             master=master,
             text=text,
-            bg="blue",
-            fg="yellow",
+            bg=IowColors.green1,
+            fg=IowColors.blue1,
             command=command   
         )
         
@@ -117,9 +122,28 @@ class SetButton(tk.Button):
         tk.Button.__init__(self,
             master=master,
             text=text,
-            command=command   
+            command=command,
+            bg = IowColors.grey1,
+            fg = "white"
         )
         
+class FrameTitleLabel(tk.Label):
+    def __init__(self, text, master=None):
+        tk.Label.__init__(self,
+            master=master,
+            text=text, 
+            bg = IowColors.blue1, 
+            fg = 'white'
+        )
+        self.config(font=("Meat Plus", 24))
+        
+class Frame(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self,
+            master=master,
+            bg = IowColors.blue1
+        )
+    
 def read_iow_esm_configuration(file_name):
     
     config = {}
@@ -133,6 +157,7 @@ def read_iow_esm_configuration(file_name):
 class IowEsmGui:
     def __init__(self):
         self.window = tk.Tk(className="IOW_ESM")
+        self.window.configure(background=IowColors.blue1)
         #self.window.geometry("500x500")
         
         self.labels = {}
@@ -140,13 +165,18 @@ class IowEsmGui:
         self.buttons = {}
         self.texts = {}
         self.frames = {}
+        self.messages = {}
         
         self.restart = False
         
-        self.labels["greeting"] = tk.Label(text="Welcome to the IOW_ESM GUI!")
+        self.functions = IowEsmFunctions(self)
+        
+        self.labels["greeting"] = tk.Label(text="Welcome to the IOW_ESM GUI!", bg = IowColors.blue1, fg = 'white')
         self.labels["greeting"].pack()
         
-        self.functions = IowEsmFunctions(self)
+        #img = tk.PhotoImage(file=root_dir + "/gui/logo_iow_englisch_rgb.gif")
+        #self.labels["logo"] = tk.Label(image=img)
+        #self.labels["logo"].pack()
         
         self.texts["monitor"] = tk.Text()
 
@@ -248,31 +278,53 @@ class IowEsmGui:
         self.buttons["store_" + dst_name].pack()
         
     def _build_window_clone_origins(self):
-        self.labels["clone_origins"] = tk.Label(
-            text="You are using the IOW_ESM for the first time.",
-        )
+        
+        self.frames["clone_origins"] = Frame(master=self.window)
+        
+        self.labels["clone_origins_title"] = FrameTitleLabel(master=self.frames["clone_origins"], text="You are using the IOW_ESM for the first time.")
+        self.labels["clone_origins_title"].pack()
+        
+        self.labels["clone_origins"] = tk.Label(master=self.frames["clone_origins"], text="You have to clone the origins of the components first:", bg = IowColors.blue1, fg = 'white')
         self.labels["clone_origins"].pack()
-
-        self.buttons["clone_origins"] = FunctionButton("Clone origins", self.functions.clone_origins)
+        
+        self.buttons["clone_origins"] = FunctionButton("Clone origins", self.functions.clone_origins, master=self.frames["clone_origins"])
         self.buttons["clone_origins"].pack()
+        
+        self.frames["clone_origins"].pack()
         
         self.texts["monitor"].pack()
         
     def _build_window_edit_destinations(self):
-        self.labels["edit_destinations"] = tk.Label(
-            text="You have to edit your DESTINATIONS file",
+        
+        self.frames["edit_destinations"] = Frame(master=self.window)
+        
+        self.labels["edit_destinations_title"] = FrameTitleLabel(master=self.frames["edit_destinations"], text="You are using the IOW_ESM for the first time.")
+        self.labels["edit_destinations_title"].pack()
+        
+        self.labels["edit_destinations"] = tk.Label(master=self.frames["edit_destinations"],
+            text="You have to edit your DESTINATIONS file:", bg = IowColors.blue1, fg = 'white'
         )
         self.labels["edit_destinations"].pack()
+        
+        self.frames["edit_destinations"].pack()
         
         self._edit_file(root_dir + "/DESTINATIONS.example", root_dir + "/DESTINATIONS")
         
         self.texts["monitor"].pack()
         
     def _build_window_edit_setups(self):
-        self.labels["edit_setups"] = tk.Label(
-            text="You have to edit your SETUPS file",
+        
+        self.frames["edit_setups"] = Frame(master=self.window)
+        
+        self.labels["edit_setups_title"] = FrameTitleLabel(master=self.frames["edit_setups"], text="You are using the IOW_ESM for the first time.")
+        self.labels["edit_setups_title"].pack()
+        
+        self.labels["edit_setups"] = tk.Label(master=self.frames["edit_setups"],
+            text="You have to edit your SETUPS file:",  bg = IowColors.blue1, fg = 'white'
         )
         self.labels["edit_setups"].pack()
+        
+        self.frames["edit_setups"].pack()
         
         self._edit_file(root_dir + "/SETUPS.example", root_dir + "/SETUPS")
         
@@ -281,26 +333,42 @@ class IowEsmGui:
     def _build_frame_destinations(self):
         
         # create destinations frame
-        self.frames["destinations"] = tk.Frame(master=self.window)
+        self.frames["destinations"] = Frame(master=self.window)
         
         # create objects of the frame
-        self.labels["destinations"] = tk.Label(master=self.frames["destinations"], text="Destinations")
+        self.labels["destinations"] = FrameTitleLabel(master=self.frames["destinations"], text="Destinations")
         
         for dst in self.destinations.keys():
             self.buttons["set_" + dst] = SetButton(dst, partial(self.functions.set_destination, dst), master=self.frames["destinations"])
             
-        self.labels["current_dst"] = tk.Label(text="Current destination", master=self.frames["destinations"])
+        self.labels["current_dst"] = tk.Label(text="Current destination:", master=self.frames["destinations"], bg = IowColors.blue1, fg = 'white')
         self.entries["current_dst"] = tk.Entry(master=self.frames["destinations"])
         
         # pack everything on a grid
-        columnspan = len(self.destinations)
+        max_buttons_in_row = 6
+        if len(self.destinations) < max_buttons_in_row:
+            columnspan = len(self.destinations) 
+        else:
+            columnspan = max_buttons_in_row
+        
+        row = 0
         self.labels["destinations"].grid(row=0, columnspan=columnspan)
         
+        row += 1
         for c, dst in enumerate(self.destinations.keys()):
-            self.buttons["set_" + dst].grid(row=1, column=c)
-            
-        self.labels["current_dst"].grid(row=3, columnspan=columnspan)
-        self.entries["current_dst"].grid(row=4, columnspan=columnspan)
+            if (c % max_buttons_in_row) == 0:
+                row += 1
+            self.buttons["set_" + dst].grid(row=row, column=(c % max_buttons_in_row))
+           
+        row += 1
+        self.labels["current_dst"].grid(row=row, columnspan=columnspan)
+        
+        row += 1
+        self.entries["current_dst"].grid(row=row, columnspan=columnspan)
+        
+        row += 1
+        blank = tk.Label(text="", master=self.frames["destinations"], bg = IowColors.blue1)
+        blank.grid(row=row, columnspan=columnspan)
         
         # pack the frame
         self.frames["destinations"].pack(padx='5', pady='5')
@@ -309,10 +377,10 @@ class IowEsmGui:
     def _build_frame_build(self, first_time):
         
         # create build frame
-        self.frames["build"] = tk.Frame(master=self.window)
+        self.frames["build"] = Frame(master=self.window)
         
         # label
-        self.labels["build"] = tk.Label(master=self.frames["build"], text="Build")
+        self.labels["build"] = FrameTitleLabel(master=self.frames["build"], text="Build")
         
         if first_time:
             self.buttons["build_all"] = FunctionButton("Build all", self.functions.build_origins_first_time, master=self.frames["build"])
@@ -334,6 +402,9 @@ class IowEsmGui:
                 ori_short = ori.split("/")[-1]
                 self.buttons["build_" + ori_short].grid(row=2, column=c)
         
+        blank = tk.Label(text="", master=self.frames["build"], bg = IowColors.blue1)
+        blank.grid(row=3, columnspan=columnspan)
+        
         self.frames["build"].pack(padx='5', pady='5')        
         
         if first_time:
@@ -343,15 +414,15 @@ class IowEsmGui:
     def _build_frame_setups(self, first_time):
         
         # create build frame
-        self.frames["setups"] = tk.Frame(master=self.window)
+        self.frames["setups"] = Frame(master=self.window)
         
         # title label
-        self.labels["setups"] = tk.Label(master=self.frames["setups"], text="Setups")
+        self.labels["setups"] = FrameTitleLabel(master=self.frames["setups"], text="Setups")
 
         for setup in self.setups.keys():
             self.buttons["set_" + setup] = SetButton(setup, partial(self.functions.set_setup, setup), master=self.frames["setups"])
             
-        self.labels["current_setups"] = tk.Label(text="Current setups", master=self.frames["setups"])
+        self.labels["current_setups"] = tk.Label(text="Current setups:", master=self.frames["setups"], bg = IowColors.blue1, fg = 'white')
         self.entries["current_setups"] = tk.Entry(master=self.frames["setups"])        
         self.buttons["clear_setups"] = FunctionButton("Clear setups", self.functions.clear_setups, master=self.frames["setups"])
         
@@ -373,23 +444,40 @@ class IowEsmGui:
         self.buttons["clear_setups"].grid(row=4, columnspan=columnspan)
         self.buttons["deploy_setups"].grid(row=5, columnspan=columnspan)
         
+        blank = tk.Label(text="", master=self.frames["setups"], bg = IowColors.blue1)
+        blank.grid(row=6, columnspan=columnspan)
+        
         self.frames["setups"].pack(padx='5', pady='5')
         
         if first_time:
             self.texts["monitor"].pack()
+            
+    def _build_frame_run(self):
+        
+        self.frames["run"] = Frame(master=self.window)
+        
+        self.labels["run"] = FrameTitleLabel(master=self.frames["run"], text="Run the model")
+        self.buttons["run"] = FunctionButton("Run", self.functions.run, master=self.frames["run"])
+
+        self.labels["run"].grid(row=0)
+        self.buttons["run"] .grid(row=1)
+        
+        blank = tk.Label(text="", bg = IowColors.blue1, master=self.frames["run"])
+        blank.grid(row=2)
+        
+        self.frames["run"].pack()
         
     def _build_window(self):
         
         self._build_frame_destinations()
+        
         self._build_frame_build(False)
+        
         self._build_frame_setups(False)
         
-        self.labels["run"] = tk.Label(text="Run the model")
-        self.labels["run"].pack()
-        
-        self.buttons["run"] = FunctionButton("Run", self.functions.run)
-        self.buttons["run"].pack()
-        
+        self._build_frame_run()
+
+    
         self.texts["monitor"].pack()
         
         
