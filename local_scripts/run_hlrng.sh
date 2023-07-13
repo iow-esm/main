9956204640aacc1c1747d039747b1e07bb7d1c6e
Sven Karsten <sven.karsten@io-warnemuende.de>
2023-07-13 13:35:26 +0200

    Updated to newest anaconda module

--- local_scripts/run_hlrng.sh
+++ local_scripts/run_hlrng.sh
@@ -3,7 +3,7 @@ dest_folder=$2
 prepare_before_run=${3:-false}
 setup=${4:-""}
 
-python_module="anaconda3/2019.03"
+python_module="anaconda3/2020.11"
 
 if [ ${prepare_before_run} == true ]; then
 	echo ssh -t $user_at_dest "module load ${python_module}; cd $dest_folder/scripts/prepare; python3 ./create_mappings.py ${setup}"
