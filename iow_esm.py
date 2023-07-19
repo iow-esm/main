bdf7d632f2b4329290d4348dd3a5e063b705d6c8
Sven Karsten <sven.karsten@io-warnemuende.de>
2023-07-19 15:07:59 +0200

    Catch case when gui is not installed

--- iow_esm.py
+++ iow_esm.py
@@ -4,7 +4,15 @@ Created on Thu Sep  9 13:51:29 2021
 
 @author: Sven
 """
+
 import sys
+import glob
+
+if not glob.glob('./gui'):
+    print("This script would start the graphical user interface (GUI).")
+    print("The GUI is not yet installed, please pull it from the repository or use the command-line interface.")
+    sys.exit()
+
 sys.path.append('./gui')
 from gui import *
 
