78b8f7add4ec3086d14bc975633f3cf231040661
Sven Karsten <sven.karsten@io-warnemuende.de>
2023-07-26 14:25:48 +0200

    Enabled releasing for github

--- release.sh
+++ release.sh
@@ -4,11 +4,10 @@
 ## Enter new version here            ##
 #######################################
 
-new_version="1.03.00"
+new_version="1.04.00"
 commit_message="Release $new_version
 
-This is a new version with new features and bug fixes, see Readme.
-Caution this version is not compatible to version 1.00.00 of the 8nm MOM5_Baltic setups! 
+This is a new version with new features and bug fixes, see Readme. 
 "
 
 #######################################
@@ -18,12 +17,14 @@ Caution this version is not compatible to version 1.00.00 of the 8nm MOM5_Baltic
 #######################################
 
 origins=(
-"components/OASIS3-MCT      :   1.00.01"
-"components/flux_calculator :   1.02.00"
-"components/MOM5            :   1.01.00"
-"components/CCLM            :   1.00.03"
-"tools/I2LM                 :   1.00.01"
-"postprocess                :   1.02.00"
+"components/OASIS3-MCT      :   1.00.02"
+"components/flux_calculator :   1.03.00"
+"components/MOM5            :   1.01.01"
+"components/CCLM            :   1.01.00"
+"tools/I2LM                 :   1.00.02"
+"postprocess                :   1.03.00"
+"tester                     :   1.00.00"
+"patch                      :   1.00.00"
 )
 
 #######################################
@@ -36,37 +37,59 @@ origins=(
 ## Leave untouched                   ##
 #######################################
 
-ref=`git rev-parse --verify $new_version 2>&1 | awk '{print $1}'`
-if [ "$ref" != "fatal:" ]; then
-    echo "Version $new_version already exits. Abort."
-    exit  
-fi    
+# create IOW version and public github version simultaneously
+for v in "${new_version}" "github/${new_version}"; do
 
-for ori in "${origins[@]}"; do
-    version=${ori##*:}
-    origin=${ori%:*}
-    repo=`awk -v origin=$origin '{if(origin==$1){print $2}}' ORIGINS`
-    ref=`git ls-remote --heads $repo $version | awk '{print $2}'`
-    
-    if [ -z $ref ]; then
-        echo "Version $version not found for $origin at $repo. Abort."
-        exit
+    # if we have a github version we need different origins file
+    if [[ "${v}" =~ "github/" ]]; then
+        sed s%git.io-warnemuende.de/iow_esm%github.com/iow-esm%g ORIGINS > ORIGINS.github
+        origins_file="ORIGINS.github"
+    else
+        origins_file="ORIGINS"
     fi
+
+    ref=`git rev-parse --verify $v 2>&1 | awk '{print $1}'`
+    if [ "$ref" != "fatal:" ]; then
+        echo "Version $v already exits. Abort."
+        exit  
+    fi    
+
+    for ori in "${origins[@]}"; do
+        version=${ori##*:}
+        origin=${ori%:*}
+        repo=`awk -v origin=$origin '{if(origin==$1){print $2}}' ${origins_file}`
+        ref=`git ls-remote --heads $repo $version | awk '{print $2}'`
+
+        if [ $? != 0 ]; then
+            echo "Could not get info for $version of $origin at $repo. Abort."
+            exit   
+        fi     
+        
+        if [ -z "$ref" ]; then
+            echo "Version $version not found for $origin at $repo. Abort."
+            exit
+        fi
+    done
+
+    git checkout -b ${v}
+
+    for ori in "${origins[@]}"; do
+        version=${ori##*:}
+        origin=${ori%:*}
+        repo=`awk -v origin=$origin '{if(origin==$1){print $2}}' ${origins_file}`
+        echo "$origin" "$repo" "$version" >> ORIGINS.tmp
+    done
+
+    mv ORIGINS.tmp ORIGINS
+
+    if [[ "${v}" =~ "github/" ]]; then
+        rm "ORIGINS.github" 
+    fi   
+
+    git commit -m "${commit_message}" ORIGINS
+
+    git switch -
+
 done
-
-git checkout -b ${new_version}
-
-for ori in "${origins[@]}"; do
-    version=${ori##*:}
-    origin=${ori%:*}
-    repo=`awk -v origin=$origin '{if(origin==$1){print $2}}' ORIGINS`
-    echo "$origin" "$repo" "$version" >> ORIGINS.tmp
-done
-
-mv ORIGINS.tmp ORIGINS
-
-git commit -m "${commit_message}" ORIGINS
-
-git switch -
 
 #######################################
